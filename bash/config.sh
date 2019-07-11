3a3ae76762469297de66951c250f75f17eb9edc9
dlordi <domenico.lordi@gmail.com>
2019-07-11 09:52:50 +0200

    Create config.sh

---
+++ bash/config.sh
@@ -0,0 +1,9 @@
+alias ..='cd ..'
+shopt -s histappend
+bind '"\e[A": history-search-backward'
+bind '"\e[B": history-search-forward'
+alias gta='git add -A'
+alias gtc='git commit -m'
+alias gtd='git diff'
+alias gtl='git log'
+alias gts='git status -s'
