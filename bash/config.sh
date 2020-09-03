81722db3ba2587fc3f930847db49c6c460f8467b
dlordi <domenico.lordi@gmail.com>
2020-09-03 20:50:54 +0200

    Delete config.sh

--- bash/config.sh
+++
@@ -1,9 +0,0 @@
-alias ..='cd ..'
-shopt -s histappend
-bind '"\e[A": history-search-backward'
-bind '"\e[B": history-search-forward'
-alias gta='git add -A'
-alias gtc='git commit -m'
-alias gtd='git diff'
-alias gtl='git log'
-alias gts='git status -s'
