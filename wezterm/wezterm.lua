c9a9171452a1beb86be65b7b7212e9bfd928280a
Domenico Lordi <domenico.lordi@gmail.com>
2024-07-09 08:12:32 +0200

    Get environment variable value in configuration file

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,9 +1,10 @@
+local os = require('os')
 local wezterm = require('wezterm')
 local config = {}
 
 -- BEGIN Windows-specific settings
 config.default_prog = { 'cmd.exe', '/k', '%CMDER_ROOT%\\vendor\\init.bat' }
-config.default_cwd = '%USERPROFILE%\\Desktop' -- NOTE: replace %USERPROFILE% with the actual value of the environment variable; backslashes MUST be escaped!
+config.default_cwd = os.getenv('USERPROFILE') .. '\\Desktop' -- NOTE: backslashes MUST be escaped!
 -- END Windows-specific settings
 
 config.font_size = 10
