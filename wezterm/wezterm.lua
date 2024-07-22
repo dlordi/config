f12e4cc79b925adaff81408c08320903f6875f37
Domenico Lordi <domenico.lordi@gmail.com>
2024-07-22 21:01:14 +0200

    Disable tab bar in wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,12 +1,14 @@
 local os = require('os')
 local wezterm = require('wezterm')
-local config = {}
+local config = wezterm.config_builder()
 
 -- BEGIN Windows-specific settings
 config.default_prog = { 'cmd.exe', '/k', '%CMDER_ROOT%\\vendor\\init.bat' }
 config.default_cwd = os.getenv('USERPROFILE') .. '\\Desktop' -- NOTE: backslashes MUST be escaped!
 -- END Windows-specific settings
 
+config.font = wezterm.font('JetBrainsMono Nerd Font')
 config.font_size = 10
+config.enable_tab_bar = false
 
 return config
