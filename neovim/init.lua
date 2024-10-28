5bc80669d43059c1e2fbdc16a68d8f86f619d7eb
Domenico Lordi <domenico.lordi@gmail.com>
2024-10-28 13:49:34 +0100

    Add telescope plugin for neovim

--- neovim/init.lua
+++ neovim/init.lua
@@ -74,6 +74,16 @@ require("lazy").setup({
       })
     end,
   },
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+    end,
+  },
 })
 
 vim.opt.clipboard = "unnamedplus" -- use system clipboard for copy ("yank") / paste
