1f8201651066e1021d16cc17eaa3c09f682e0e39
Domenico Lordi <domenico.lordi@gmail.com>
2024-10-29 06:41:08 +0100

    Refactor config to support OS specific settings

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,12 +2,13 @@ local os = require('os')
 local wezterm = require('wezterm')
 local config = wezterm.config_builder()
 
--- BEGIN Windows-specific settings
-config.default_prog = { 'cmd.exe', '/k', '%CMDER_ROOT%\\vendor\\init.bat' }
-config.default_cwd = os.getenv('USERPROFILE') .. '\\Desktop' -- NOTE: backslashes MUST be escaped!
--- END Windows-specific settings
+if package.config:sub(1,1) == '\\' then
+  -- Windows-specific settings
+  config.default_prog = { 'cmd.exe', '/k', '%CMDER_ROOT%\\vendor\\init.bat' }
+  config.default_cwd = os.getenv('USERPROFILE') .. '\\Desktop'
+end
 
-config.font = wezterm.font('JetBrainsMono Nerd Font')
+config.font = wezterm.font('JetBrainsMonoNL Nerd Font')
 config.font_size = 10
 config.enable_tab_bar = false
 
