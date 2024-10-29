3d529d3734cebb80cc7cf55bd71a42cc5d6b32e1
Domenico Lordi <domenico.lordi@gmail.com>
2024-10-29 13:51:57 +0100

    Maximize window on start

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,15 +1,20 @@
-local os = require('os')
-local wezterm = require('wezterm')
+local os = require("os")
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-if package.config:sub(1,1) == '\\' then
+if package.config:sub(1, 1) == "\\" then
   -- Windows-specific settings
-  config.default_prog = { 'cmd.exe', '/k', '%CMDER_ROOT%\\vendor\\init.bat' }
-  config.default_cwd = os.getenv('USERPROFILE') .. '\\Desktop'
+  config.default_prog = { "cmd.exe", "/k", "%CMDER_ROOT%\\vendor\\init.bat" }
+  config.default_cwd = os.getenv("USERPROFILE") .. "\\Desktop"
 end
 
-config.font = wezterm.font('JetBrainsMonoNL Nerd Font')
+config.font = wezterm.font("JetBrainsMonoNL Nerd Font")
 config.font_size = 10
 config.enable_tab_bar = false
+
+wezterm.on("gui-startup", function(cmd)
+  local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
 
 return config
