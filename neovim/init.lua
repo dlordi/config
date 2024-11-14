52c72ef69aec5867d3291e2308c6d2e4516ae178
Domenico Lordi <domenico.lordi@gmail.com>
2024-11-14 19:55:08 +0100

    Add telescope keymap to show keymaps :-)

--- neovim/init.lua
+++ neovim/init.lua
@@ -83,6 +83,7 @@ require("lazy").setup({
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
       vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+      vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Telescope keymaps" })
     end,
   },
 })
