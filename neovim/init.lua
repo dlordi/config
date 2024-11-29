0ed948b28abdf826c24a8ae2e2b89faf00efeb8f
Domenico Lordi <domenico.lordi@gmail.com>
2024-11-29 08:30:48 +0100

    Add C languange support to neovim treesitter

--- neovim/init.lua
+++ neovim/init.lua
@@ -41,7 +41,7 @@ require("lazy").setup({
     build = ":TSUpdate",
     config = function()
       require("nvim-treesitter.configs").setup({
-        ensure_installed = { "lua", "python", "javascript", "json", "markdown", "markdown_inline", "go" },
+        ensure_installed = { "lua", "python", "javascript", "json", "markdown", "markdown_inline", "go", "c" },
         sync_install = false,
         highlight = { enable = true },
         indent = { enable = true },
