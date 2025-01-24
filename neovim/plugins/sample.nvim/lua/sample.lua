9cffaffa7ecdc8ea29a7421517ca5df2257b377d
Domenico Lordi <domenico.lordi@gmail.com>
2025-01-24 19:14:20 +0100

    Develop neovim sample plugin into a buffer selection plugin

--- neovim/plugins/sample.nvim/lua/sample.lua
+++ neovim/plugins/sample.nvim/lua/sample.lua
@@ -4,18 +4,22 @@ M.setup = function()
   print('sample.nvim loaded')
 end
 
+local on_quit = function()
+  local win = 0
+  local buf = vim.api.nvim_win_get_buf(win)
+  vim.api.nvim_buf_delete(buf, { force = true })
+  pcall(vim.api.nvim_win_close, win, true)
+end
+
 local on_select = function()
   local win = 0
   local buf = vim.api.nvim_win_get_buf(win)
   local cursor = vim.api.nvim_win_get_cursor(win)
   local row = cursor[1] -- 1-indexed
   -- local col = cursor[2] -- 0-indexed
-  print(vim.api.nvim_buf_get_lines(buf, 0, -1, true)[row])
-end
-
-local on_quit = function()
-  local win = 0
-  pcall(vim.api.nvim_win_close, win, true)
+  local line = vim.api.nvim_buf_get_lines(buf, 0, -1, true)[row]
+  on_quit() -- close current window/buffer BEFORE next one is activated!
+  vim.api.nvim_set_current_buf(tonumber(string.match(line, '([^:]+)')))
 end
 
 local create_win_config = function()
@@ -23,7 +27,7 @@ local create_win_config = function()
   local height = math.floor(vim.o.lines * 0.8)
 
   return {
-    title = 'Title of sample.nvim',
+    title = ' buffers (sample.nvim) ',
     relative = 'editor',
     style = 'minimal',
     border = 'rounded',
@@ -34,17 +38,23 @@ local create_win_config = function()
   }
 end
 
-M.sample = function(lines)
+M.sample = function()
+  local lines = {}
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    local name = vim.api.nvim_buf_get_name(buf)
+    table.insert(lines, buf .. ': ' .. name)
+  end
+
   local buf = vim.api.nvim_create_buf(false, true)
   vim.api.nvim_buf_set_lines(buf, 0, -1, true, lines)
   -- modifiable must be disabled AFTER its content has been set/changed!
   vim.api.nvim_set_option_value('modifiable', false, { buf = buf })
-  vim.api.nvim_buf_add_highlight(buf, -1, 'String', 0, 0, -1)
-  vim.keymap.set('n', 'x', on_select, { buffer = buf })
-  vim.keymap.set('n', 'q', on_quit, { buffer = buf })
+  -- vim.api.nvim_buf_add_highlight(buf, -1, 'String', 0, 0, -1)
+  vim.keymap.set('n', '<Enter>', on_select, { buffer = buf })
+  vim.keymap.set('n', '<Esc>', on_quit, { buffer = buf })
 
   local win = vim.api.nvim_open_win(buf, true, create_win_config())
-  vim.api.nvim_create_autocmd('WinLeave', { callback = on_quit })
+  vim.api.nvim_create_autocmd('WinLeave', { callback = on_quit, buffer = buf })
   vim.api.nvim_create_autocmd('VimResized', {
     group = vim.api.nvim_create_augroup('sample-VimResized', {}),
     callback = function()
@@ -57,9 +67,6 @@ M.sample = function(lines)
   })
 end
 
--- vim.keymap.set('n', '<Leader>la', function()
---   local lines = { 'sample.nvim line 1', 'sample.nvim line 2', 'sample.nvim line 3' }
---   M.sample(lines)
--- end)
+vim.keymap.set('n', '<Leader>ls', M.sample)
 
 return M
