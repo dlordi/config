f0dbb1b3f66461b00e2302efbf1d1ea8b24f561c
dlordi <domenico.lordi@gmail.com>
2026-01-19 21:36:40 +0100

    Automate silent git install on Windows

--- windows/20-software.cmd
+++ windows/20-software.cmd
@@ -4,3 +4,8 @@ winget install --source winget --exact --id DominikReichl.KeePass
 winget install --source winget --exact --id Mozilla.Firefox.it
 winget install --source winget --exact --id MartinBresson.Executor --include-unknown
 winget install --source winget --exact --id fjsoft.MyPhoneExplorer
+
+@REM references:
+@REM - https://github.com/microsoft/winget-cli/discussions/3462
+@REM - https://gitforwindows.org/silent-or-unattended-installation.html
+winget install --accept-source-agreements --accept-package-agreements --source winget --exact --id Git.Git --custom "/VERYSILENT /NORESTART /NOCANCEL /LOADINF=git_install_options.ini"
