185be75db8449929b7ea76478687c64cad621159
Domenico Lordi <domenico.lordi@gmail.com>
2026-01-30 00:02:12 +0100

    Fix applying Linux configuration

--- apply.sh
+++ apply.sh
@@ -1,17 +1,26 @@
 #!/usr/bin/env bash
 
+echo "`date +%H:%M:%S` applying default configurations..."
+
 # PATH_TO_THIS_REPO does not end with a slash (the directory separator)!
 PATH_TO_THIS_REPO=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # alacritty
+echo -n "`date +%H:%M:%S`   - alacritty... "
 [ -e $HOME/.config/alacritty ] && rm -rf $HOME/.config/alacritty
+mkdir -p $HOME/.config
 ln -sf $PATH_TO_THIS_REPO/alacritty $HOME/.config/alacritty
+echo "done"
 
 # neovim
+echo -n "`date +%H:%M:%S`   - neovim... "
 [ -e $HOME/.config/nvim ] && rm -rf $HOME/.config/nvim
+mkdir -p $HOME/.config
 ln -sf $PATH_TO_THIS_REPO/neovim $HOME/.config/nvim
+echo "done"
 
 # vim
+echo -n "`date +%H:%M:%S`   - vim... "
 mkdir -p "$HOME/.vim"
 ln -sf "$PATH_TO_THIS_REPO/vim/vimrc" "$HOME/.vim/vimrc"
 [ -e $HOME/.vim/colors ] && rm -rf $HOME/.vim/colors
@@ -19,9 +28,13 @@ ln -sf "$PATH_TO_THIS_REPO/vim/colors" "$HOME/.vim/colors"
 mkdir -p ~/.vim/pack/tpope/start
 [ ! -d ~/.vim/pack/tpope/start/vim-fugitive ] && git -C ~/.vim/pack/tpope/start clone https://github.com/tpope/vim-fugitive
 git -C ~/.vim/pack/tpope/start/vim-fugitive pull >/dev/null
+echo "done"
 
 # yazi
+echo -n "`date +%H:%M:%S`   - yazi... "
 [ -e $HOME/.config/yazi ] && rm -rf $HOME/.config/yazi
+mkdir -p $HOME/.config
 ln -sf $PATH_TO_THIS_REPO/yazi $HOME/.config/yazi
+echo "done"
 
-echo configuration applied successfully!
+echo "`date +%H:%M:%S` configuration applied successfully!"
