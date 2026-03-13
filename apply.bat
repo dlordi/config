06f8c08fab6e82dc23aa91aedfa56f647bc702e0
dlordi <domenico.lordi@gmail.com>
2026-03-13 07:32:35 +0100

    Refactoring to get current time

--- apply.bat
+++ apply.bat
@@ -3,27 +3,18 @@
 @REM NOTE: PATH_TO_THIS_REPO ends with a backslash (the directory separator)!
 set PATH_TO_THIS_REPO=%~dp0
 
-set HH=%TIME:~0,2%
-if "%HH:~0,1%" == " " set HH=0%HH:~1,1%
-set MM=%TIME:~3,2%
-set SS=%TIME:~6,2%
+call :set_HH_MM_SS
 echo %HH%:%MM%:%SS% applying default configurations...
 
 @REM alacritty
-set HH=%TIME:~0,2%
-if "%HH:~0,1%" == " " set HH=0%HH:~1,1%
-set MM=%TIME:~3,2%
-set SS=%TIME:~6,2%
+call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - alacritty... "
 if exist "%APPDATA%\alacritty" rd "%APPDATA%\alacritty"
 mklink /D "%APPDATA%\alacritty" "%PATH_TO_THIS_REPO%\alacritty" >NUL
 echo done
 
 @REM autohotkey: compile my-autohotkeys.ahk to executable into the startup folder
-set HH=%TIME:~0,2%
-if "%HH:~0,1%" == " " set HH=0%HH:~1,1%
-set MM=%TIME:~3,2%
-set SS=%TIME:~6,2%
+call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - autohotkey... "
 if exist "%LOCALAPPDATA%\Programs\AutoHotkey\Compiler\Ahk2Exe.exe" (
 	echo|set /p _="compiling my-autohotkeys (if prompted, choose to RELOAD)... "
@@ -36,39 +27,27 @@ if exist "%LOCALAPPDATA%\Programs\AutoHotkey\Compiler\Ahk2Exe.exe" (
 echo done
 
 @REM capsicain
-set HH=%TIME:~0,2%
-if "%HH:~0,1%" == " " set HH=0%HH:~1,1%
-set MM=%TIME:~3,2%
-set SS=%TIME:~6,2%
+call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - capsicain... "
 if exist C:\bin\capsicain\capsicain.ini del C:\bin\capsicain\capsicain.ini
 if exist C:\bin\capsicain mklink C:\bin\capsicain\capsicain.ini "%PATH_TO_THIS_REPO%\capsicain\capsicain.ini" >NUL
 echo done
 
 @REM cmder
-set HH=%TIME:~0,2%
-if "%HH:~0,1%" == " " set HH=0%HH:~1,1%
-set MM=%TIME:~3,2%
-set SS=%TIME:~6,2%
+call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - cmder... "
 powershell -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -File "%PATH_TO_THIS_REPO%\apply-cmder.ps1" "%PATH_TO_THIS_REPO%\cmder" "%PATH_TO_THIS_REPO%"
 echo done
 
 @REM helix
-set HH=%TIME:~0,2%
-if "%HH:~0,1%" == " " set HH=0%HH:~1,1%
-set MM=%TIME:~3,2%
-set SS=%TIME:~6,2%
+call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - helix... "
 if exist "%APPDATA%\helix" rd "%APPDATA%\helix"
 mklink /D "%APPDATA%\helix" "%PATH_TO_THIS_REPO%\helix" >NUL
 echo done
 
 @REM git
-set HH=%TIME:~0,2%
-if "%HH:~0,1%" == " " set HH=0%HH:~1,1%
-set MM=%TIME:~3,2%
-set SS=%TIME:~6,2%
+call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - git... "
 powershell -ExecutionPolicy Bypass -NoLogo -NoProfile -File "%PATH_TO_THIS_REPO%\apply-git.ps1" "%PATH_TO_THIS_REPO%\git\gitconfig"
 if not exist "%USERPROFILE%\.config\git" mkdir "%USERPROFILE%\.config\git"
@@ -77,10 +56,7 @@ mklink "%USERPROFILE%\.config\git\git-prompt.sh" "%PATH_TO_THIS_REPO%\git\git-pr
 echo done
 
 @REM lazygit
-set HH=%TIME:~0,2%
-if "%HH:~0,1%" == " " set HH=0%HH:~1,1%
-set MM=%TIME:~3,2%
-set SS=%TIME:~6,2%
+call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - lazygit... "
 if not exist "%APPDATA%\lazygit" mkdir "%APPDATA%\lazygit"
 if exist "%APPDATA%\lazygit\config.yml" del "%APPDATA%\lazygit\config.yml"
@@ -88,20 +64,14 @@ mklink "%APPDATA%\lazygit\config.yml" "%PATH_TO_THIS_REPO%\lazygit\config.yml" >
 echo done
 
 @REM neovim
-set HH=%TIME:~0,2%
-if "%HH:~0,1%" == " " set HH=0%HH:~1,1%
-set MM=%TIME:~3,2%
-set SS=%TIME:~6,2%
+call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - neovim... "
 if exist "%LOCALAPPDATA%\nvim" rd "%LOCALAPPDATA%\nvim"
 mklink /D "%LOCALAPPDATA%\nvim" "%PATH_TO_THIS_REPO%\neovim" >NUL
 echo done
 
 @REM ruff
-set HH=%TIME:~0,2%
-if "%HH:~0,1%" == " " set HH=0%HH:~1,1%
-set MM=%TIME:~3,2%
-set SS=%TIME:~6,2%
+call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - ruff... "
 if not exist "%USERPROFILE%\.config" mkdir "%USERPROFILE%\.config"
 if exist "%USERPROFILE%\.config\ruff.toml" del "%USERPROFILE%\.config\ruff.toml"
@@ -109,10 +79,7 @@ mklink "%USERPROFILE%\.config\ruff.toml" "%PATH_TO_THIS_REPO%\ruff\ruff.toml" >N
 echo done
 
 @REM vim
-set HH=%TIME:~0,2%
-if "%HH:~0,1%" == " " set HH=0%HH:~1,1%
-set MM=%TIME:~3,2%
-set SS=%TIME:~6,2%
+call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - vim... "
 if exist "%USERPROFILE%\.vim\vimrc" del "%USERPROFILE%\.vim\vimrc"
 if exist "%USERPROFILE%\.vim\colors" rd "%USERPROFILE%\.vim\colors"
@@ -125,10 +92,7 @@ git -C "%USERPROFILE%\.vim\pack\tpope\start\vim-fugitive" pull >NUL
 echo done
 
 @REM vscodium
-set HH=%TIME:~0,2%
-if "%HH:~0,1%" == " " set HH=0%HH:~1,1%
-set MM=%TIME:~3,2%
-set SS=%TIME:~6,2%
+call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - vscodium... "
 if exist "%APPDATA%\VSCodium\User\settings.json" del "%APPDATA%\VSCodium\User\settings.json"
 if exist "%APPDATA%\VSCodium\User\keybindings.json" del "%APPDATA%\VSCodium\User\keybindings.json"
@@ -138,10 +102,7 @@ mklink "%APPDATA%\VSCodium\User\keybindings.json" "%PATH_TO_THIS_REPO%\vscodium\
 echo done
 
 @REM yazi
-set HH=%TIME:~0,2%
-if "%HH:~0,1%" == " " set HH=0%HH:~1,1%
-set MM=%TIME:~3,2%
-set SS=%TIME:~6,2%
+call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - yazi... "
 if not exist "%APPDATA%\yazi" mkdir "%APPDATA%\yazi"
 if exist "%APPDATA%\yazi\config" rd "%APPDATA%\yazi\config"
@@ -149,48 +110,33 @@ mklink /D "%APPDATA%\yazi\config" "%PATH_TO_THIS_REPO%\yazi" >NUL
 echo done
 
 @REM wezterm
-set HH=%TIME:~0,2%
-if "%HH:~0,1%" == " " set HH=0%HH:~1,1%
-set MM=%TIME:~3,2%
-set SS=%TIME:~6,2%
+call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - wezterm... "
-if exist "%USERPROFILE%\.config\wezterm\wezterm.lua" del "%USERPROFILE%\.config\wezterm\wezterm.lua"
 if not exist "%USERPROFILE%\.config\wezterm" mkdir "%USERPROFILE%\.config\wezterm"
+if exist "%USERPROFILE%\.config\wezterm\wezterm.lua" del "%USERPROFILE%\.config\wezterm\wezterm.lua"
 mklink "%USERPROFILE%\.config\wezterm\wezterm.lua" "%PATH_TO_THIS_REPO%\wezterm\wezterm.lua" >NUL
 echo done
 
 @REM windows
-set HH=%TIME:~0,2%
-if "%HH:~0,1%" == " " set HH=0%HH:~1,1%
-set MM=%TIME:~3,2%
-set SS=%TIME:~6,2%
+call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - windows... "
 reg import "%PATH_TO_THIS_REPO%\windows\10-prefs.reg" 2>NUL
 echo done
 
 @REM winmerge
-set HH=%TIME:~0,2%
-if "%HH:~0,1%" == " " set HH=0%HH:~1,1%
-set MM=%TIME:~3,2%
-set SS=%TIME:~6,2%
+call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - winmerge... "
 reg import "%PATH_TO_THIS_REPO%\winmerge\settings.reg" 2>NUL
 echo done
 
 @REM wt (windows terminal)
-set HH=%TIME:~0,2%
-if "%HH:~0,1%" == " " set HH=0%HH:~1,1%
-set MM=%TIME:~3,2%
-set SS=%TIME:~6,2%
+call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - wt (Windows Terminal)... "
 powershell -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -File "%PATH_TO_THIS_REPO%\apply-wt.ps1" "%PATH_TO_THIS_REPO%\wt"
 echo done
 
 @REM zed
-set HH=%TIME:~0,2%
-if "%HH:~0,1%" == " " set HH=0%HH:~1,1%
-set MM=%TIME:~3,2%
-set SS=%TIME:~6,2%
+call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - zed... "
 if exist "%APPDATA%\Zed\snippets" rd "%APPDATA%\Zed\snippets"
 if exist "%APPDATA%\Zed\settings.json" del "%APPDATA%\Zed\settings.json"
@@ -201,8 +147,14 @@ mklink "%APPDATA%\Zed\settings.json" "%PATH_TO_THIS_REPO%\zed\settings.json" >NU
 mklink "%APPDATA%\Zed\keymap.json" "%PATH_TO_THIS_REPO%\zed\keymap.json" >NUL
 echo done
 
+call :set_HH_MM_SS
+echo %HH%:%MM%:%SS% configurations successfully applied
+
+goto :EOF
+
+:set_HH_MM_SS
 set HH=%TIME:~0,2%
 if "%HH:~0,1%" == " " set HH=0%HH:~1,1%
 set MM=%TIME:~3,2%
 set SS=%TIME:~6,2%
-echo %HH%:%MM%:%SS% configurations successfully applied
+goto :EOF
