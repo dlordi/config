523de373e14fac37f45df04851a648409b0e852f
dlordi <domenico.lordi@gmail.com>
2026-03-19 21:53:56 +0100

    Allows to apply specific configurations as command parameter

--- apply.bat
+++ apply.bat
@@ -6,14 +6,47 @@ set PATH_TO_THIS_REPO=%~dp0
 call :set_HH_MM_SS
 echo %HH%:%MM%:%SS% applying default configurations...
 
+if "%1"=="" (
+	call :alacritty
+	call :autohotkey
+	call :capsicain
+	call :cmder
+	call :helix
+	call :git
+	call :lazygit
+	call :neovim
+	call :ruff
+	call :tabby
+	call :vim
+	call :vscodium
+	call :yazi
+	call :waveterm
+	call :wezterm
+	call :windows
+	call :winmerge
+	call :wt
+	call :zed
+) else (
+	call :%1
+)
+
+call :set_HH_MM_SS
+echo %HH%:%MM%:%SS% configurations successfully applied
+
+goto :EOF
+
+
 @REM alacritty
+:alacritty
 call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - alacritty... "
 if exist "%APPDATA%\alacritty" rd "%APPDATA%\alacritty"
 mklink /D "%APPDATA%\alacritty" "%PATH_TO_THIS_REPO%\alacritty" >NUL
 echo done
+goto :EOF
 
 @REM autohotkey: compile my-autohotkeys.ahk to executable into the startup folder
+:autohotkey
 call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - autohotkey... "
 if exist "%LOCALAPPDATA%\Programs\AutoHotkey\Compiler\Ahk2Exe.exe" (
@@ -25,28 +58,36 @@ if exist "%LOCALAPPDATA%\Programs\AutoHotkey\Compiler\Ahk2Exe.exe" (
 		/base "%LOCALAPPDATA%\Programs\AutoHotkey\v2\AutoHotkey64.exe"
 )
 echo done
+goto :EOF
 
 @REM capsicain
+:capsicain
 call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - capsicain... "
 if exist C:\bin\capsicain\capsicain.ini del C:\bin\capsicain\capsicain.ini
 if exist C:\bin\capsicain mklink C:\bin\capsicain\capsicain.ini "%PATH_TO_THIS_REPO%\capsicain\capsicain.ini" >NUL
 echo done
+goto :EOF
 
 @REM cmder
+:cmder
 call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - cmder... "
 powershell -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -File "%PATH_TO_THIS_REPO%\apply-cmder.ps1" "%PATH_TO_THIS_REPO%\cmder" "%PATH_TO_THIS_REPO%"
 echo done
+goto :EOF
 
 @REM helix
+:helix
 call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - helix... "
 if exist "%APPDATA%\helix" rd "%APPDATA%\helix"
 mklink /D "%APPDATA%\helix" "%PATH_TO_THIS_REPO%\helix" >NUL
 echo done
+goto :EOF
 
 @REM git
+:git
 call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - git... "
 powershell -ExecutionPolicy Bypass -NoLogo -NoProfile -File "%PATH_TO_THIS_REPO%\apply-git.ps1" "%PATH_TO_THIS_REPO%\git\gitconfig"
@@ -54,38 +95,48 @@ if not exist "%USERPROFILE%\.config\git" mkdir "%USERPROFILE%\.config\git"
 if exist "%USERPROFILE%\.config\git\git-prompt.sh" del "%USERPROFILE%\.config\git\git-prompt.sh"
 mklink "%USERPROFILE%\.config\git\git-prompt.sh" "%PATH_TO_THIS_REPO%\git\git-prompt.sh" >NUL
 echo done
+goto :EOF
 
 @REM lazygit
+:lazygit
 call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - lazygit... "
 if not exist "%APPDATA%\lazygit" mkdir "%APPDATA%\lazygit"
 if exist "%APPDATA%\lazygit\config.yml" del "%APPDATA%\lazygit\config.yml"
 mklink "%APPDATA%\lazygit\config.yml" "%PATH_TO_THIS_REPO%\lazygit\config.yml" >NUL
 echo done
+goto :EOF
 
 @REM neovim
+:neovim
 call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - neovim... "
 if exist "%LOCALAPPDATA%\nvim" rd "%LOCALAPPDATA%\nvim"
 mklink /D "%LOCALAPPDATA%\nvim" "%PATH_TO_THIS_REPO%\neovim" >NUL
 echo done
+goto :EOF
 
 @REM ruff
+:ruff
 call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - ruff... "
 if not exist "%USERPROFILE%\.config" mkdir "%USERPROFILE%\.config"
 if exist "%USERPROFILE%\.config\ruff.toml" del "%USERPROFILE%\.config\ruff.toml"
 mklink "%USERPROFILE%\.config\ruff.toml" "%PATH_TO_THIS_REPO%\ruff\ruff.toml" >NUL
 echo done
+goto :EOF
 
 @REM tabby
+:tabby
 call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - tabby... "
 if exist "%APPDATA%\tabby\config.yaml" del "%APPDATA%\tabby\config.yaml"
 mklink "%APPDATA%\tabby\config.yaml" "%PATH_TO_THIS_REPO%\tabby\config.yaml" >NUL
 echo done
+goto :EOF
 
 @REM vim
+:vim
 call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - vim... "
 if exist "%USERPROFILE%\.vim\vimrc" del "%USERPROFILE%\.vim\vimrc"
@@ -97,8 +148,10 @@ if not exist "%USERPROFILE%\.vim\pack\tpope\start" mkdir "%USERPROFILE%\.vim\pac
 if not exist "%USERPROFILE%\.vim\pack\tpope\start\vim-fugitive" git -C "%USERPROFILE%\.vim\pack\tpope\start" clone https://github.com/tpope/vim-fugitive >NUL
 git -C "%USERPROFILE%\.vim\pack\tpope\start\vim-fugitive" pull >NUL
 echo done
+goto :EOF
 
 @REM vscodium
+:vscodium
 call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - vscodium... "
 if exist "%APPDATA%\VSCodium\User\settings.json" del "%APPDATA%\VSCodium\User\settings.json"
@@ -107,51 +160,65 @@ if not exist "%APPDATA%\VSCodium\User" mkdir "%APPDATA%\VSCodium\User
 mklink "%APPDATA%\VSCodium\User\settings.json" "%PATH_TO_THIS_REPO%\vscodium\settings.json" >NUL
 mklink "%APPDATA%\VSCodium\User\keybindings.json" "%PATH_TO_THIS_REPO%\vscodium\keybindings.json" >NUL
 echo done
+goto :EOF
 
 @REM yazi
+:yazi
 call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - yazi... "
 if not exist "%APPDATA%\yazi" mkdir "%APPDATA%\yazi"
 if exist "%APPDATA%\yazi\config" rd "%APPDATA%\yazi\config"
 mklink /D "%APPDATA%\yazi\config" "%PATH_TO_THIS_REPO%\yazi" >NUL
 echo done
+goto :EOF
 
 @REM waveterm
+:waveterm
 call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - wave terminal... "
 if not exist "%USERPROFILE%\.config\waveterm" mkdir "%USERPROFILE%\.config\waveterm"
 if exist "%USERPROFILE%\.config\waveterm\settings.json" del "%USERPROFILE%\.config\waveterm\settings.json"
 mklink "%USERPROFILE%\.config\waveterm\settings.json" "%PATH_TO_THIS_REPO%\waveterm\settings.json" >NUL
 echo done
+goto :EOF
 
 @REM wezterm
+:wezterm
 call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - wezterm... "
 if not exist "%USERPROFILE%\.config\wezterm" mkdir "%USERPROFILE%\.config\wezterm"
 if exist "%USERPROFILE%\.config\wezterm\wezterm.lua" del "%USERPROFILE%\.config\wezterm\wezterm.lua"
 mklink "%USERPROFILE%\.config\wezterm\wezterm.lua" "%PATH_TO_THIS_REPO%\wezterm\wezterm.lua" >NUL
 echo done
+goto :EOF
 
 @REM windows
+:windows
 call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - windows... "
 reg import "%PATH_TO_THIS_REPO%\windows\10-prefs.reg" 2>NUL
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
 echo done
+goto :EOF
 
 @REM winmerge
+:winmerge
 call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - winmerge... "
 reg import "%PATH_TO_THIS_REPO%\winmerge\settings.reg" 2>NUL
 echo done
+goto :EOF
 
 @REM wt (windows terminal)
+:wt
 call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - wt (Windows Terminal)... "
 powershell -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -File "%PATH_TO_THIS_REPO%\apply-wt.ps1" "%PATH_TO_THIS_REPO%\wt"
 echo done
+goto :EOF
 
 @REM zed
+:zed
 call :set_HH_MM_SS
 echo|set /p _="%HH%:%MM%:%SS%   - zed... "
 if exist "%APPDATA%\Zed\snippets" rd "%APPDATA%\Zed\snippets"
@@ -162,10 +229,6 @@ mklink /D "%APPDATA%\Zed\snippets" "%PATH_TO_THIS_REPO%\zed\snippets" >NUL
 mklink "%APPDATA%\Zed\settings.json" "%PATH_TO_THIS_REPO%\zed\settings.json" >NUL
 mklink "%APPDATA%\Zed\keymap.json" "%PATH_TO_THIS_REPO%\zed\keymap.json" >NUL
 echo done
-
-call :set_HH_MM_SS
-echo %HH%:%MM%:%SS% configurations successfully applied
-
 goto :EOF
 
 :set_HH_MM_SS
