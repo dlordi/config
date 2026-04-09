3ef3ba07a9475843d6346a2dc69069219783adc3
dlordi <domenico.lordi@gmail.com>
2026-04-10 01:59:19 +0200

    Add documentation on sshd config on Windows

--- apply.ps1
+++ apply.ps1
@@ -94,7 +94,7 @@ function Format-Json {
     $regexUnlessQuoted = '(?=([^"]*"[^"]*")*[^"]*$)'
 
     $result = $Json -split '\r?\n' | ForEach-Object {
-        # If the line contains a ] or } character, 
+        # If the line contains a ] or } character,
         # we need to decrement the indentation level, unless:
         #   - it is inside quotes, AND
         #   - it does not contain a [ or {
@@ -105,7 +105,7 @@ function Format-Json {
         # Replace all colon-space combinations by ": " unless it is inside quotes.
         $line = (' ' * $indent) + ($_.TrimStart() -replace ":\s+$regexUnlessQuoted", ': ')
 
-        # If the line contains a [ or { character, 
+        # If the line contains a [ or { character,
         # we need to increment the indentation level, unless:
         #   - it is inside quotes, AND
         #   - it does not contain a ] or }
---
+++ sshd/README.md
@@ -0,0 +1,9 @@
+# sshd
+
+## Windows
+
+### OpenSSH by Microsoft
+
+- see https://learn.microsoft.com/en-us/windows-server/administration/openssh/openssh-server-configuration
+
+- config file is in `%ProgramData%\ssh\sshd_config`
